cadf5811ceb0f27a124e3f798d8a47ac5d9e167a
Mauricio Vuljevas <mvuljevas@outlook.com>
2019-10-21 11:25:06 -0300

    Minor Version Improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ $(".randomize").click(function() {
     var random_pick = Math.floor(Math.random() * walkers.length);
 
     winner      = walkers[random_pick].walker;
-    pnt_winner  = $('.winner-name').html(winner);
+    pnt_winner  = $('.the-winner').html(winner);
 
     $('.winner-name').removeClass('active');
     $('.overlay-confeti').removeClass('active');
@@ -32,10 +32,11 @@ $(".randomize").click(function() {
   }
 });
 
+
 $(".overlay-confeti").click(function() {
     $('.winner-name').removeClass('active');
     $('.overlay-confeti').removeClass('active');
-    confetti.stop(); 
+    confetti.stop();
 });
 
 //Add Walker
@@ -103,4 +104,39 @@ $('#walker-add').on('click',function(e){
 //             clearInterval(animate);
 //         }
 //     }, 1000);
+// });
+
+
+// $(function () {
+//     $("#upload").bind("click", function () {
+//         var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+//         if (regex.test($("#fileUpload").val().toLowerCase())) {
+//             if (typeof (FileReader) != "undefined") {
+//                 var reader = new FileReader();
+//                 reader.onload = function (e) {
+//                     var table = $("<table />");
+//                     var rows = e.target.result.split("\n");
+//                     for (var i = 0; i < rows.length; i++) {
+//                         var row = $("<tr />");
+//                         var cells = rows[i].split(",");
+//                         if (cells.length > 1) {
+//                             for (var j = 0; j < cells.length; j++) {
+//                                 var cell = $("<td />");
+//                                 cell.html(cells[j]);
+//                                 row.append(cell);
+//                             }
+//                             table.append(row);
+//                         }
+//                     }
+//                     $("#dvCSV").html('');
+//                     $("#dvCSV").append(table);
+//                 }
+//                 reader.readAsText($("#fileUpload")[0].files[0]);
+//             } else {
+//                 alert("This browser does not support HTML5.");
+//             }
+//         } else {
+//             alert("Please upload a valid CSV file.");
+//         }
+//     });
 // });
